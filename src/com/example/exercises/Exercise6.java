d5032ee626e60cf4de3867040036ecf0332c6099
Zack Di Salvo <zdisalvo@gmail.com>
2024-03-20 00:25:42 -0700

    completed exercise 6 using built in country&city count class, also completed it creating a LinkedHashMap then sorting it in reverse order

--- src/com/example/exercises/Exercise6.java
+++ src/com/example/exercises/Exercise6.java
@@ -1,10 +1,15 @@
 package com.example.exercises;
 
 import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import com.example.dao.CountryDao;
 import com.example.dao.InMemoryWorldDao;
+import com.example.domain.Country;
+
 
 /**
  * 
@@ -16,13 +21,61 @@ public class Exercise6 {
 
 	public static void main(String[] args) {
 		// Sort the countries by number of their cities in descending order
-		Function<CountryCityCountPair, Integer> countExtracter = CountryCityCountPair::count;
-		var countriesWithCityCountInDescOrder = countryDao.findAllCountries()
-		          .stream()
-		          .map(country -> new CountryCityCountPair(country,country.getCities().size()))
-		          .sorted(Comparator.comparing(countExtracter).reversed())
-		          .toList();
-		countriesWithCityCountInDescOrder.forEach(System.out::println);
+//		Function<CountryCityCountPair, Integer> countExtracter = CountryCityCountPair::count;
+//		var countriesWithCityCountInDescOrder = countryDao.findAllCountries()
+//		          .stream()
+//		          .map(country -> new CountryCityCountPair(country,country.getCities().size()))
+//		          .sorted(Comparator.comparing(countExtracter).reversed())
+//		          .toList();
+//		countriesWithCityCountInDescOrder.forEach(System.out::println);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//		var countriesSortedByNumberOfCities = countryDao.findAllCountries().stream()
+//				.map(country -> new CountryCityCountPair(country, country.getCities().size()))
+//				.sorted(Comparator.comparingInt(CountryCityCountPair::count).reversed());
+//
+//		countriesSortedByNumberOfCities.forEach(
+//				countryCityCountPair -> System.out.printf("\n%s has \t%d cities", countryCityCountPair.country().getName(), countryCityCountPair.count())
+//		);
+
+//		countriesSortedByNumberOfCities.forEach(
+//				countryCityCountPair -> System.out.printf("\n%s has \t%d cities", countryCityCountPair.country().getName(), countryCityCountPair.count())
+//		);
+
+
+		var countriesSortedByNumberOfCities = countryDao.findAllCountries().stream()
+				.collect(Collectors.toMap(
+						country -> country.getName(),
+						country -> country.getCities().size()
+				)).entrySet().stream()
+				.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+				.collect(Collectors.toMap(
+						Map.Entry::getKey,
+						Map.Entry::getValue,
+						(oldValue, newValue) -> oldValue,
+						LinkedHashMap::new
+				));
+
+		//var countriesSortedByNumberOfCities = countriesByNumberOfCities.entrySet().stream()
+
+		countriesSortedByNumberOfCities.forEach(
+				(key, value) -> System.out.printf("\n%s has \t%d cities",key, value)
+		);
+
 	}
 
 }
